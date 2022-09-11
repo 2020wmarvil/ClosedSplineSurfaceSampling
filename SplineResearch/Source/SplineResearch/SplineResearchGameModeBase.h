729b93316ab6bb4346a804cbfdf1f16c4fb654ed
Wyatt Marvil <2020wmarvil@gmail.com>
2022-09-11 18:55:42 -0400

    Set up spline project

---
+++ SplineResearch/Source/SplineResearch/SplineResearchGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "SplineResearchGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SPLINERESEARCH_API ASplineResearchGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
